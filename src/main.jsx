f108dfd4a15b913e8bafad9ffd1fc4deda0d11df
aunmegustas <cralperezhe@ittepic.edu.mx>
2022-10-11 13:25:54 -0600

    8. renderizar las rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import Expenses from './routes/expenses'
+import Invoices from './routes/invoices'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App/>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path="expenses" element={<Expenses/>}/>
+      <Route path="invoices" element={<Invoices/>}/>
+    </Routes>
   </BrowserRouter>
 )
