0a35aa41871ad3f3b3278871b84d6e6ecb9a9fd7
aunmegustas <cralperezhe@ittepic.edu.mx>
2022-10-11 13:21:39 -0600

    6. Añadir algunos enlaces, invoices, expenses

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <h1>Bookkeeper!</h1>
+      <nav style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}>
+        <Link to="/invoices">Invoices</Link> | {" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   )
 }
